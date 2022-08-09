df9f72ada55f66650a1c6b4192eb98dff0dcbde7
CaioVitor1 <caiovitor33@hotmail.com>
2022-08-09 16:09:59 -0300

    add repositories

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import GlobalStyle from "./styles/GlobalStyle";
 import Timeline from "./Timeline";
 
 export default function App() {
     
     return (
-        
+       <>
+        <GlobalStyle />
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Timeline />} /> 
             </Routes>
         </BrowserRouter>
-        
+        </>
     )
 }

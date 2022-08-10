b2997f99f5ac99618e2ec13c2a27cf7fcb110e50
Tutuezin <vinnycamara1@gmail.com>
2022-08-10 19:54:22 -0300

    refactor: delete example file and change timeline route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from "react";
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NewPost from "./Pages/NewPost"
+import NewPost from "./Pages/NewPost";
 import GlobalStyle from "./styles/GlobalStyle";
-import Timeline from "./Timeline";
+import Timeline from "./Pages/home/Timeline";
 import Register from "./Pages/auth/Register";
 import Login from "./Pages/auth/Login";
 
 export default function App() {
-
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Timeline />} />
-          <Route path="signin" element={<Login />} />;
+          <Route path="/" element={<Login />} />;
           <Route path="signup" element={<Register />} />;
+          <Route path="/timeline" element={<Timeline />} />
           <Route path="/newPost" element={<NewPost />} />
         </Routes>
       </BrowserRouter>
     </>
   );
 }
-

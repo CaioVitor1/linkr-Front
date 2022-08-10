a5f4f45d7208544084fe018a6dd37d3821ed17cc
Tutuezin <vinnycamara1@gmail.com>
2022-08-10 17:07:33 -0300

    feat: register page with requests

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import Timeline from "./Timeline";
 import SignIn from "./Pages/auth/Login";
+import Register from "./Pages/auth/Register";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Timeline />} />
           <Route path="signin" element={<SignIn />} />;
+          <Route path="signup" element={<Register />} />;
         </Routes>
       </BrowserRouter>
     </>

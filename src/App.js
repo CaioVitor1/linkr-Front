7c277d869e94026e981db68e2b7683f7f81c546e
EricChandia <eric.chandia.ti@gmail.com>
2022-08-10 20:46:32 -0300

    trending sync with back

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import Timeline from "./Timeline";
-import Trending from "./Pages/trending/Trending";
+import Trending from "./pages/trending/Trending";
 
 export default function App() {
     
